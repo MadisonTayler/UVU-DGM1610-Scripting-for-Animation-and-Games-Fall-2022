e45dc8285c98814c89c01b1d77f8eca6b62e369d
Madison Tayler <10939791@my.uvu.edu>
2022-11-13 11:47:01 -0700

    Added Player Health Script
    - Created player health script
    - Added player health script to player

---
+++ 2D Platformer/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+    public float deathDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int dmgAmount)
+    {
+        currentHealth -= dmgAmount;
+        
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject,deathDelay);
+        }
+    }
+
+    public void AddHealth(int healAmount)
+    {
+        currentHealth +=  healAmount;
+
+        if(currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
