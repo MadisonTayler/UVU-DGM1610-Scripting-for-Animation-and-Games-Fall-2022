4a9f4b836de783e2cb512628b7aeb37180351407
Madison Tayler <10939791@my.uvu.edu>
2022-12-06 12:48:58 -0700

    Added Flag and Game Manager Scripts
    - Created flag using pro builder
    - Flag disappears once picked up and reappears when placed

---
+++ CTF/CTF/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Flag Stats")]
+    public bool hasFlag;
+    public bool flagPlaced;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Flag bools
+        hasFlag = false;
+        flagPlaced = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flagPlaced == true)
+        {
+            WinGame();
+        }
+    }
+
+    public void PlaceFlag()
+    {
+        flagPlaced = true;
+    }
+
+    void WinGame()
+    {
+        Debug.Log("You've won!");
+        // Time.timeScale = 0; // Freeze the game
+        // Show win screen
+        // GameUI.instance.SetEndGameScreen(true, curScore);
+    }
+}
